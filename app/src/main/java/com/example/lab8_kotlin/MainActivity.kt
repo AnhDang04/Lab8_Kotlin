07c37ab2b15f49134fe0bc43ea0e2e50a2d54847
HaiDang04 <dangbhhph33065@fpt.edu.vn>
2024-06-09 21:09:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab8_kotlin/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.lab8_kotlin
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kot104.ui.theme.KOT104_Lab8_thanghtph31577Theme
+import com.example.lab8_kotlin.ui.screen.ScreenNavigation
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KOT104_Lab8_thanghtph31577Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ScreenNavigation()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KOT104_Lab8_thanghtph31577Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/lab8_kotlin/entities/ListType.kt
@@ -0,0 +1,5 @@
+package com.example.lab8_kotlin.entities
+
+enum class ListType {
+    ROW, COLUMN, GRID
+}
